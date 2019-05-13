ce19f120f96a1cc7ec35b7a17c3b0446841a34ac
Frugtoy <borislesin@yandex.ru>
2019-05-13 21:49:11 +0200

    smsh

--- Makefile
+++ Makefile
@@ -1,13 +1,12 @@
 all: lay
-
-lay: node.o stack.o main.o
-	g++ node.o stack.o main.o -o lay
+lay: node.o stack.o sort_machine.o main.o
+	g++ node.o stack.o sort_machine.o main.o -o lay
 node.o:node.cpp
 	g++ -c node.cpp
-
 stack.o:stack.cpp
 	g++ -c stack.cpp
-
+sort_machine.o:sort_machine.cpp
+	g++ -c sort_machine.cpp
 main.o:main.cpp
 	g++ -c main.cpp
 
--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 
 #include<iostream>
-#include "stack.cpp"
+#include"sort_machine.h"
 using std::cin;
 using std::cout;
 using std::endl;
-
-
-
 int main()
 {
-    Stack <int>s;
-    s.push(5);
+  //  Stack <int>s;
+   /* s.push(5);
     s.push(3);
     s.push(2);
     s.push(1221);
     while (s.get_tail()!=nullptr)
     cout<<" "<<s.pop()<<endl;
+   */
+  SortingStation s;
+  s.parse("2+2*2");
+  
+
 return 0;
 }
